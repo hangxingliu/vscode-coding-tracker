1afefc96e020e63bf5296ee7dcb9cdd06423fefa
LiuYue <hangxingliu@gmail.com>
2016-11-07 07:06:02 +0800

    restructured the intellisense files and add dependencies to package.json

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/colors/index.d.ts" />
-/// <reference path="globals/fs-extra/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/request/index.d.ts" />
