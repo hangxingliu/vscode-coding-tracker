9ae6bc7de9690887e261da4a7f92690804c1a208
LiuYue <hangxingliu@gmail.com>
2017-01-27 02:25:18 +0800

    remove server side codes

--- server/lib/Handler404and500.js
+++
@@ -1,8 +0,0 @@
-returnError = (res, errInfo) => res.json({ error: errInfo}).end();
-
-module.exports = function (expressApp) {
-	//404
-	expressApp.use((req, res) => returnError(res, 'Request invalid') );
-	//500
-	expressApp.use((err, req, res, next) => Log.error(err.stack) + returnError(res, 'Server inner error') );
-};
